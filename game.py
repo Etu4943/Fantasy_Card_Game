3f21ca5f74a647654706c5f93f0cd326338fff11
Clement <clem_pot@ikmail.com>
2026-06-29 10:09:49 +0200

    Minor updates

--- game.py
+++ game.py
@@ -5,8 +5,20 @@ from card import Card
 import random
 import uuid
 
-NB_PEOPLE = 4 
+NB_PEOPLE = 8 
 NB_CARD_BY_HAND = 7
+
+CARDS = [
+	["farfadet", "Pouvoir 1"],
+	["dryade", "Pouvoir 2"],
+	["elfe", "Pouvoir 3"],
+	# Fee disabled until I found a better architecture 
+	# Like I need to be able to play it while the opponent is playing
+	# ["fee", "Pouvoir 4"],
+	["gnome", "Pouvoir 5"],
+	["korrigan", "Pouvoir 6"],
+	["lutin", "Pouvoir 7"]
+]
 
 def init_hand(room_code, user_id): # 7 cartes de chaque !
 	for _ in range(NB_CARD_BY_HAND):
@@ -19,22 +31,9 @@ def add_card_to_hand(room_code, user_id):
 
 def init_deck(room_code):
 	deck[room_code] = []
-	for _ in range(NB_PEOPLE):
-		deck[room_code].append(Card("farfadet", "Pouvoir 1", str(uuid.uuid4())))
-	for _ in range(NB_PEOPLE):
-		deck[room_code].append(Card("dryade", "Pouvoir 2", str(uuid.uuid4())))
-	for _ in range(NB_PEOPLE):
-		deck[room_code].append(Card("elfe", "Pouvoir 3", str(uuid.uuid4())))
-	# Fee disabled until I found a better architecture 
-	# Like I need to be able to play it while the opponent is playing
-	# for _ in range(NB_PEOPLE):
-	# 	deck[room_code].append(Card("fee", "Pouvoir 4", str(uuid.uuid4())))
-	for _ in range(NB_PEOPLE):
-		deck[room_code].append(Card("gnome", "Pouvoir 5", str(uuid.uuid4())))
-	for _ in range(NB_PEOPLE):
-		deck[room_code].append(Card("korrigan", "Pouvoir 6", str(uuid.uuid4())))
-	for _ in range(NB_PEOPLE):
-		deck[room_code].append(Card("lutin", "Pouvoir 7", str(uuid.uuid4())))
+	for card in CARDS :
+		for _ in range(NB_PEOPLE) :
+			deck[room_code].append(Card(card[0], card[1], str(uuid.uuid4())))
 	random.shuffle(deck[room_code])
 
 	
